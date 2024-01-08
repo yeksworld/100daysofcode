bdcb8e450b7d585d151b9231f6f2e9dc4a8b2c66
yunus <yunusemre4634@gmail.com>
2024-01-08 12:54:59 +0200

    FizzBuzz game

---
+++ FizzBuzz.py
@@ -0,0 +1,10 @@
+x: int = 0
+for x in range(1, 101):
+    if x % 3 == 0 and x % 5 == 0:
+        print("FizzBuzz")
+    elif x % 3 == 0:
+        print("Fizz")
+    elif x % 5 == 0:
+        print("Buzz")
+    else:
+        print(x)
